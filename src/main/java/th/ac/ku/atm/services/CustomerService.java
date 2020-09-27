bc933567fb6e255bc938607d7678efe6b1171a2c
issaraphap <issaraphap12@gmail.com>
2020-09-27 22:01:41 +0700

    “JDBC”

--- src/main/java/th/ac/ku/atm/services/CustomerService.java
+++ src/main/java/th/ac/ku/atm/services/CustomerService.java
@@ -1,58 +1,52 @@
 package th.ac.ku.atm.services;
 
-import org.springframework.stereotype.Service;
-import th.ac.ku.atm.model.Customer;
 import org.mindrot.jbcrypt.BCrypt;
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
+import th.ac.ku.atm.model.Customer;
+
 import java.util.List;
 
 @Service
 public class CustomerService {
+    private CustomerRepository repository;
 
-    private ArrayList<Customer> customerList;
-
-
-    @PostConstruct
-    public void postConstruct(){
-        this.customerList = new ArrayList<>();
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
 
-    public void createCustomer(Customer customer){
-        //hash pin for customer
+    public void createCustomer(Customer customer) {
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
-    public List<Customer> getCustomers(){
-        return new ArrayList<>(this.customerList);
+    public Customer findCustomer(int id) {
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
+    public List<Customer> getCustomers() {
+        return repository.findAll();
+    }
+
+    public Customer checkPin(Customer inputCustomer) {
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+
+        if (storedCustomer != null) {
+            String storedPin = storedCustomer.getPin();
+            if (BCrypt.checkpw(inputCustomer.getPin(), storedPin))
+                return storedCustomer;
+        }
+        return null;
+    }
     private String hash(String pin) {
         String salt = BCrypt.gensalt(12);
-        return BCrypt.hashpw(pin,salt);
+        return BCrypt.hashpw(pin, salt);
     }
-
-    public Customer findCustomer(int id){
-        for(Customer customer:customerList){
-            if(customer.getId() == id){
-                return customer;
-            }
-        }
-        return null;
-    }
-
-    public Customer checkPin(Customer loginCustomer){
-        Customer storedCustomer = findCustomer(loginCustomer.getId());
-        if(storedCustomer != null){
-            String hashPin = storedCustomer.getPin();
-
-            if(BCrypt.checkpw(loginCustomer.getPin(), hashPin)){
-                return storedCustomer;
-            }
-        }
-        return null;
-    }
-
 }
+
